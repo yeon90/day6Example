8ee605a16d5b12e28526a7701b638db43d9005da
jiyeon park <yeon900610@gmail.com>
2017-01-11 18:36:17 +0900

    ConvertMeasurement

--- Measurement/Measurement/ViewController.swift
+++ Measurement/Measurement/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var input : UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func convert(_ sender: Any) {
+        var inputA = Double(input.text!)
+        var celsiusA = Measurement(value: inputA!, unit: UnitTemperature.celsius)
+        
+        celsiusA.convert(to: UnitTemperature.fahrenheit)
+       
+        result.text = String(celsiusA.value)
 
+    }
+    
+  
 }
-
